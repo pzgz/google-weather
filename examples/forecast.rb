75b42b41d2e1a15e398d5c42180b19aedc7e2cd6
Leon Guan <leon.guan@gmail.com>
2012-08-10 10:37:52 +0800

    Bump the httparty version to the latest

--- examples/forecast.rb
+++ examples/forecast.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../lib/google_weather'
 require 'pp'
 
-weather = GoogleWeather.new(46544, :locale => 'zh-cn')
+weather = GoogleWeather.new([32.060255,118.796877]  , :locale => 'zh-cn')
 
 forecast = weather.forecast_conditions[0]
 puts forecast.day_of_week, forecast.low, forecast.high, forecast.condition
